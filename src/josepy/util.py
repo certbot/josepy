9d6d2c64427daaab8a1742bf5f7d5151f263b46a
Adrien Ferrand <ferrand.ad@gmail.com>
2021-09-14 01:46:42 +0200

    Add comment

--- src/josepy/util.py
+++ src/josepy/util.py
@@ -11,6 +11,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec, rsa
 
 
+# Deprecated. Please use built-in decorators @classmethod and abc.abstractmethod together instead.
 def abstractclassmethod(func: Callable):
     return classmethod(abc.abstractmethod(func))
 
